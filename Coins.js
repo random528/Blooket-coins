346300d730caab7aa255d5fea6e1af6a60549f2f
random528 <ashleydanielledennis@icloud.com>
2024-10-18 22:06:38 -0400

    Update Coins.js

--- Coins.js
+++ Coins.js
@@ -9,8 +9,8 @@ async function addCurrencies() {
     const tokens = Number(prompt('How many tokens do you want to add to your account? (1000 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 1000) {
-        alert('You can only add up to 1000 tokens daily.')
+    if (tokens > 10000000) {
+        alert('You can only add up to 10000000 tokens daily.')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
